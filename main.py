f6717933c7f0c6ef2e0223b4ae0b3f360b46bb7e
Pedro Hernandez de la Rosa <pedrohernandexz904@gmail.com>
2024-03-25 15:00:04 -0600

    Se agrego el main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hola mundo</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
