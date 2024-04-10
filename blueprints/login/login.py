21f8694022e0da572d30dc3413a9b71c206e2c5c
Pedro Hernandez de la Rosa <pedrohernandexz904@gmail.com>
2024-04-10 16:12:21 -0600

    Se agregaron medidas de seguridad

--- blueprints/login/login.py
+++ blueprints/login/login.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from blueprints.usuario.model_usuario import Usuario
 from .login_form import loginForm
 from .function.auth import Autenticador
-
+from .function.intentos_f import desactivar_cuenta
 
 login_bp = Blueprint("login", __name__, template_folder="templates")
 
@@ -12,18 +12,25 @@ def login():
     form_auth = loginForm(request.form)
     messages = ''
     alert = ''
-    contador = session['contador']
     
     if request.method == 'POST':
         messages, alert, user_id = Autenticador().login(form_auth.correo.data, form_auth.pwd.data)
+        messages, contador = desactivar_cuenta().intentos()
+        
         if alert == 'warning':
-            
+            if contador > 3:
+             desactivar_cuenta().desactivar(user_id)
+             flash(messages)
+             return render_template('login.html', form=form_auth, n=alert)
+            if messages == '':
+              messages = 'Nombre de usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.'
             flash(messages) 
             return render_template('login.html', form=form_auth, n=alert)
         elif alert == 'success':
+            session['contador'] = 0  
             user = Usuario.query.get(user_id)
             login_user(user)
-            return render_template('menu.html', username = user.nombre)  
+            return render_template('menu.html', username=current_user.nombre , rol = current_user.rol)  
             
     return render_template('login.html', form=form_auth, n=alert)
 
